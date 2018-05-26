81e5d7af30b57e7b2733ae30e723a870f084a84d
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-26 16:31:15 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  if block_given?
+    i = 0
+    new_collection = []
+    while i < array.length
+      new_collection << yield(array[i].split(" ").first)
+      i += 1
+    end
+    new_collection
+  else
+    "No block given"
+  end
+end
